f7b06cd84659e88c99ebd319fa3f9b855328d261
MRE-IGU <mre-igu@outlook.com>
2019-04-28 18:55:46 +0300

    about us page updated

--- js/aboutus.js
+++ js/aboutus.js
@@ -33,7 +33,7 @@ window.addEventListener("load", function(event) {
       ],
       duration: 1900,
       opacity: 1,
-      delay: 1000,
+      delay: 800,
       easing: 'easeOutElastic(1, 3.3)',
       loop: false
     });
@@ -44,7 +44,7 @@ window.addEventListener("load", function(event) {
         ],
         duration: 1900,
         opacity: 1,
-        delay: 2000,
+        delay: 1000,
         easing: 'easeOutElastic(1, 3.3)',
         loop: false
       });
@@ -55,7 +55,7 @@ window.addEventListener("load", function(event) {
         ],
         duration: 1900,
         opacity: 1,
-        delay: 2000,
+        delay: 2700,
         easing: 'easeOutElastic(1, 3.3)',
         loop: false
       });
@@ -66,9 +66,75 @@ window.addEventListener("load", function(event) {
         ],
         duration: 1900,
         opacity: 1,
-        delay: 2000,
+        delay: 1800,
         easing: 'easeOutElastic(1, 3.3)',
         loop: false
       });
+
+
+      var b = document.getElementsByClassName("au-01-title-2");
+      var c = document.getElementsByClassName("au-01-text-2");
+      var d = document.getElementsByClassName("au-01-image-2");
+    anime({
+        targets: b,
+        keyframes: [
+          {translateX:-100},
+        ],
+        duration: 1900,
+        opacity: 1,
+        delay: 3000,
+        easing: 'easeOutElastic(1, 3.3)',
+        loop: false
+      });
+      anime({
+        targets: c,
+        keyframes: [
+          {translateX:30},
+        ],
+        duration: 1900,
+        opacity: 1,
+        delay: 4300,
+        easing: 'easeOutElastic(1, 3.3)',
+        loop: false
+      });
+      anime({
+        targets: d,
+        keyframes: [
+          {translateX:-500},
+        ],
+        duration: 1900,
+        opacity: 1,
+        delay: 3800,
+        easing: 'easeOutElastic(1, 3.3)',
+        loop: false
+      });
+
+      var x = document.getElementsByClassName("images-logo-2");
+      anime({
+          targets: x,
+          keyframes: [
+            {translateX:330},
+          ],
+          duration: 3900,
+          opacity: 1,
+          delay: 3000,
+          easing: 'easeOutElastic(0.1, 3.3)',
+          loop: false
+        });
+        var x = document.getElementsByClassName("images-text-2");
+          anime({
+          targets: x,
+          keyframes: [
+            {translateX:-330},
+          ],
+          duration: 3900,
+          opacity: 1,
+          delay: 3000,
+          easing: 'easeOutElastic(0.1, 3.3)',
+          loop: false
+        });
+
+ 
 });
+
 
